1e5b7dbc3439b772f80f7dd40c1ae66147fa2b7a
milan88888 <chaudharymilan996@gmail.com>
2026-09-15 13:19:59 +0545

    #1508 Fix - Login and account pages served a cached logged in notice to logged out visitors

--- includes/shortcodes/class-ur-shortcode-my-account.php
+++ includes/shortcodes/class-ur-shortcode-my-account.php
@@ -57,6 +57,10 @@ class UR_Shortcode_My_Account {
 	public static function output( $atts ) {
 
 		global $wp, $post;
+
+		// The account screen and the login form it falls back to are both user specific.
+		UR_Cache_Helper::disable_page_cache( 'my-account' );
+
 		wp_enqueue_script( 'tooltipster' );
 		wp_enqueue_script( 'user-registration' );
 		wp_enqueue_script( 'ur-common' );
